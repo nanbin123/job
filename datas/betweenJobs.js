fa41be22a35b479065f26c28a91ef71f35b249ed
nanbin <735552503@qq.com>
2020-07-14 14:48:28 +0800

    no message

---
+++ datas/betweenJobs.js
@@ -0,0 +1,24 @@
+let betweenJob_list = {
+      "code":  0,
+      "count":  0,
+      "data":  [
+            {
+                  "text":  "离职-随时到岗",
+                  "value":  "16"
+            },
+            {
+                  "text":  "在职-暂不考虑",
+                  "value":  "17"
+            },
+            {
+                  "text":  "在职-考虑机会",
+                  "value":  "18"
+            },
+            {
+                  "text":  "在职-月内到岗",
+                  "value":  "19"
+            }
+      ],
+      "msg":  "数据加载成功"
+}
+module.exports = { betweenJob_list }
